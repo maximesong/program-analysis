4927dbf63a641cdd008d39ecbf3e6d8afb577971
jeffrey <jeffrey@jeffrey-pc.(none)>
2013-12-03 21:59:44 +0800

    modify c++ input

--- web/server.py
+++ web/server.py
@@ -1,4 +1,6 @@
 import web
+import os
+import subprocess
 
 app = web.auto_application()
 render = web.template.render('templates/')
@@ -10,8 +12,15 @@ class index(app.page):
     def POST(self):
         mystring = web.input(code="")
         code = mystring.code;
+        filename = "myfile.cpp"
+        file_object = open(filename,'w')
+        file_object.write(code)
+        file_object.close()
+        proc = subprocess.Popen(["../pa","myfile.cpp"],stdout=subprocess.PIPE)
+        proc.wait()
+        output=proc.stdout.read()
         json = '{"nodes": ["SWH","LMY","ZYL","ZJJ"],"edges": [["ZJJ", "SWH"],["ZJJ", "LMY"],["ZJJ", "ZYL"],["SWH", "LMY"],["SWH", "ZYL"]]}'
-        return render.index(code,json)
+        return render.index(output,json)
 
 if __name__ == '__main__':
 	app.run()
