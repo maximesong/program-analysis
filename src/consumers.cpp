06cd8827c2e159cc0ee1baa45ea75f988d22e3ca
Maxime Song <me@cppdo.com>
2013-12-03 21:53:20 +0800

    output json, but need to further process at python server side

--- src/consumers.cpp
+++ src/consumers.cpp
@@ -1,39 +1,44 @@
 #include "consumers.h"
 
 #include <iostream>
+#include "jsonxx.h"
 
 using namespace std;
+using namespace jsonxx;
 
 void CallGraphConsumer::HandleTranslationUnit(ASTContext &Context)
 {
 	CallGraph callGraph;
 	callGraph.addToCallGraph(Context.getTranslationUnitDecl());
 	SourceManager &manager = Context.getSourceManager();
+	Object all;
+	Array edges;
+	Array nodes;
 	for (auto e : callGraph) {
 		if (e.first != 0) {
 			string source_file = 
 				manager.getFilename(e.first->getSourceRange().getBegin()).data();
 			if (source_file == "input.cc") {
+				string caller = to_string((long) e.second);
+				nodes << caller;
 				//e.first->dump();
-				cout << "S" << endl;
-				e.second->dump();
-				cout << " : " << endl;
 				/*
 				for (auto i = e.second->begin(); i != e.second->end(); ++i) {
 					i->dump();
 				}
 				*/
+				Array callees;
 				for (auto i : *e.second) {
-					cout << "EE: " << endl;
-					i->dump();
-					cout << "\t" << endl;
+					callees << to_string((long) i);
+					edges << Object(caller, callees);
 				}
-				cout << endl;
 			}
-
 			//cout << i.first->getSourceRange() << endl;
 		}
 	}
+	all << "nodes" << nodes;
+	all << "edges" << edges;
+	cout << all.json() << endl;
 }
 
 void CFGConsumer::HandleTranslationUnit(ASTContext &Context)
