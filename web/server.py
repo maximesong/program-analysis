f6007a4f2a3a33d560c1cf87f0c0aab1531c51cd
Maxime Song <me@cppdo.com>
2013-12-04 16:17:58 +0800

    echo code when response

--- web/server.py
+++ web/server.py
@@ -10,7 +10,66 @@ render = web.template.render('templates/', base='layout')
 class index(app.page):
     path = '/'
     def GET(self):
-        return render.index("","")
+        code = """
+#include <iostream>
+using namespace std;
+
+template<class T>
+void tprint(T t) {
+	cout << t << endl;
+}
+
+class A {
+public:
+	int bar() {
+		bar();
+		tprint(1);
+		return 2;
+	}
+};
+
+namespace B {
+
+	void bar() 
+	{
+	}
+
+}
+
+void bar()
+{
+}
+
+int foo() {
+	A a;
+	a.bar();
+	return 0;
+}
+
+void foo2(double, long) {
+}
+
+double foo(int a) {
+	foo2(0.0, 1);
+	return 3.14;
+}
+
+int main() {
+	foo();
+	foo(1);
+	bar();
+	B::bar();
+	int a = 2;
+	if (a > 0) {
+		//cout << a << endl;
+	} else {
+	//	cout << a - 1 << endl;
+	}
+	return 0;
+}
+"""
+        return render.index(code, "", "")
+
     def POST(self):
         mystring = web.input(code="")
         code = mystring.code;
@@ -35,7 +94,7 @@ class index(app.page):
 		for callee in calls[caller]:
 			output_json["edges"].append([ caller, callee ])
 	print output_json
-        return render.index(output, json.dumps(output_json))
+        return render.index(code, json.dumps(output_json), output)
 
 class dir(app.page):
     path = '/dir'
