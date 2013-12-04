57cd145deb49841bda0c043e48204054afc36485
Maxime Song <me@cppdo.com>
2013-12-04 16:34:41 +0800

    put sample code for web interface into a file

--- web/server.py
+++ web/server.py
@@ -7,68 +7,13 @@ import json
 app = web.auto_application()
 render = web.template.render('templates/', base='layout')
 
+f = open("sample_code.cpp")
+sample_code = f.read()
+
 class index(app.page):
     path = '/'
     def GET(self):
-        code = """
-#include <iostream>
-using namespace std;
-
-template<class T>
-void tprint(T t) {
-	cout << t << endl;
-}
-
-class A {
-public:
-	int bar() {
-		bar();
-		tprint(1);
-		return 2;
-	}
-};
-
-namespace B {
-
-	void bar() 
-	{
-	}
-
-}
-
-void bar()
-{
-}
-
-int foo() {
-	A a;
-	a.bar();
-	return 0;
-}
-
-void foo2(double, long) {
-}
-
-double foo(int a) {
-	foo2(0.0, 1);
-	return 3.14;
-}
-
-int main() {
-	foo();
-	foo(1);
-	bar();
-	B::bar();
-	int a = 2;
-	if (a > 0) {
-		//cout << a << endl;
-	} else {
-	//	cout << a - 1 << endl;
-	}
-	return 0;
-}
-"""
-        return render.index(code, "", "")
+        return render.index(sample_code, "", "")
 
     def POST(self):
         mystring = web.input(code="")
