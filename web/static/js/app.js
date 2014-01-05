1ed966b4b2cf45b7f715332d18a93c2046125c14
Maxime <me@cppdo.com>
2014-01-05 16:12:31 +0800

    update ui

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,33 +1,36 @@
 (function() {
-    angular.module("app", [])
-	.controller("sourceCtrl", function($scope, $http, $location) {
-	    $scope.dir = {
-		'files':[],
-		'dirs':[]
-	    };
-	    $http.get("/listdir").success(function (data) {
-		$scope.current_dir = "/";
-		$scope.dir_info = data;
-	    });
+  angular.module("app", [])
+    .controller("sourceCtrl", function($scope, $http, $location) {
+      $scope.dir = {
+	'files':[],
+	'dirs':[]
+      };
+      if (!$location.path()) {
+        $location.path("/");
+      }
 
-	    $scope.refresh = function() {
-	    }
+      $scope.refresh = function() {
+      };
 
-	    $scope.is_root_dir = function() {
-		return $scope.dir_info && $scope.dir_info.base === $scope.dir_info.parent;
-	    }
+      $scope.is_root_dir = function() {
+	return $scope.dir_info && $scope.dir_info.base === $scope.dir_info.parent;
+      };
 
-	    $scope.$watch(function () {
-		return $location.path();
-	    }, function (newPath) {
-		if (newPath == "" || newPath.endsWith("/")) {
-		    $http.post("/listdir",
-			       { "dir": newPath }).success(function (data) {
-				   $scope.dir_info = data;
-			       });
-		} else {
-		    console.log(newPath, "File");
-		}
-	    });
-	});
-})()
+      $scope.$watch(
+        function () {
+	  return $location.path();
+        },
+        function (newPath) {
+          $scope.current_dir = newPath;
+	  if (newPath == "" || newPath.endsWith("/")) {
+	    $http.post("/listdir",
+		       { "dir": newPath }
+                      ).success(function (data) {
+		        $scope.dir_info = data;
+		      });
+	  } else {
+	    console.log(newPath, "File");
+	  }
+        });
+    });
+})();
